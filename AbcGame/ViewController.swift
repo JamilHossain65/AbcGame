e6ccb1f3d01a3d5a798a4ad3d8f53bbcb2515806
“Jamil” <jmlhossain65@gmail.com>
2026-08-14 21:54:10 +0600

    project launch

---
+++ AbcGame/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AbcGame
+//
+//  Created by Jamil on 14/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world...")
+    }
+}
+
---
+++ AbcGameTests/AbcGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  AbcGameTests.swift
+//  AbcGameTests
+//
+//  Created by Jamil on 14/8/26.
+//
+
+import Testing
+@testable import AbcGame
+
+struct AbcGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
