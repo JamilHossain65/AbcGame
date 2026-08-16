7d27982312da408769f4d825b5f64696a45fd0a5
“Jamil” <jmlhossain65@gmail.com>
2026-08-16 12:46:01 +0600

    xcode cloud config

--- AbcGameTests/ViewController.swift
+++ AbcGameTests/ViewController.swift
@@ -11,7 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world...")
+
+        let demoButton = UIButton(type: .detailDisclosure)
+        demoButton.frame = CGRectMake(100, 230, 60, 40)
+        demoButton.addTarget(self, action: #selector(playAction), for: .touchUpInside)
+        view.addSubview(demoButton)
+    }
+    
+    @objc func playAction(sender: UIButton) {
+    
+        let alert = UIAlertController(title: "Alert", message: "This is a test message.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            print("The OK alert occured")
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
