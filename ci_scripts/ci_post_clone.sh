e8ab108908cf2f40ec62dc49d5bbddf3f6a35acd
“Jamil” <jmlhossain65@gmail.com>
2026-08-18 10:54:16 +0600

    cloud install script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Navigate to the repository root directory
+#cd ..
+# Install CocoaPods dependencies using Homebrew
+brew install cocoapods
+
+# Run pod install to resolve and pull your dependencies
+pod install
